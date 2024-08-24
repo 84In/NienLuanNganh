8d9f0f216c8742599ba4b2acdad3e5073bdfb378
pmss0168 <pmss0168@gmail.com>
2024-08-24 13:01:47 +0700

    update: UserController, RoleController

---
+++ WebShop/src/main/java/com/nienluan/webshop/utils/DateUtils.java
@@ -0,0 +1,22 @@
+package com.nienluan.webshop.utils;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Slf4j
+public class FormatDate {
+    @Bean
+    public Date formatDate(String date) {
+        try{
+            return new SimpleDateFormat("yyyy-MM-dd").parse(date);
+        }
+        catch (ParseException e){
+            log.error(e.getMessage());
+        }
+    }
+}
