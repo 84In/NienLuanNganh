f8a6dce0bd165a7522368ad342eb9c5416a280ee
pmss0168 <pmss0168@gmail.com>
2024-09-15 14:14:55 +0700

    modify: chagne little pieces in Review

--- client/src/components/Product/Review.js
+++ client/src/components/Product/Review.js
@@ -5,8 +5,8 @@ import ReviewItem from "./ReviewItem";
 
 const Review = () => {
   //Assumed value
-  const totalReviews = 74;
   const ratings = [59, 14, 1, 0, 0];
+  const totalReviews = ratings.reduce((acc, item) => acc + item, 0);
   const rating = {
     name: "SANS",
     star: 5,
@@ -20,24 +20,43 @@ const Review = () => {
         <Grid2 item xs={12} md={4} className="flex flex-col gap-2">
           <h1 className="text-lg font-semibold">Tổng quan</h1>
           <div className="flex w-full items-center gap-2">
-            <p className="text-5xl font-bold">{averageRating}</p>
-            <Rating value={averageRating} precision={0.5} size="large" readOnly />
+            <p className="text-5xl font-bold">{averageRating ? averageRating : 0}</p>
+            <Rating value={averageRating ? averageRating : 0} precision={0.5} size="large" readOnly />
           </div>
-          <p className="text-gray-500">({totalReviews} đánh giá)</p>
+          <p className="text-gray-500">({totalReviews ? totalReviews : 0} đánh giá)</p>
         </Grid2>
-        <Grid2 item xs={12} md={6} sx={{ width: "100%" }}>
-          {ratings.map((item, index) => (
-            <div className="flex items-center gap-2" key={index}>
-              <Rating value={5 - index} precision={0.5} size="small" readOnly />
-              <LinearProgress
-                variant="determinate"
-                color="primary"
-                value={(item / totalReviews) * 100}
-                sx={{ height: "8px", width: "40%", backgroundColor: "#e0e0e0", borderRadius: "10px" }}
-              />
-              <p>{item}</p>
-            </div>
-          ))}
+        <Grid2
+          item
+          xs={12}
+          md={6}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            alignItems: { xs: "normal", md: "end" },
+            width: "100%",
+          }}
+        >
+          <div className="w-full">
+            {ratings.map((item, index) => (
+              <div className="flex items-center gap-2" key={index}>
+                <Rating value={5 - index} precision={0.5} size="small" readOnly />
+                <LinearProgress
+                  variant="determinate"
+                  color="primary"
+                  value={(item / totalReviews) * 100}
+                  sx={{
+                    height: "8px",
+                    width: "60%",
+                    minWidth: "125px",
+                    backgroundColor: "#e0e0e0",
+                    borderRadius: "10px",
+                  }}
+                />
+                <p>{item}</p>
+              </div>
+            ))}
+          </div>
         </Grid2>
       </Grid2>
       <hr className="mt-4 flex h-[2px] w-full items-center justify-center bg-gray-400 px-4" />
