f0a6e89f76b4ed4a916dba0940cc3b73d6137612
84In <trungtin7166293@gmail.com>
2024-10-03 23:02:01 +0700

    fix: size grid

--- client/src/containers/System/AdminHome.js
+++ client/src/containers/System/AdminHome.js
@@ -26,42 +26,46 @@ const AdminHome = ({ user }) => {
     setIsOpen(!isOpen);
   };
 
-  // Effect để theo dõi kích thước cửa sổ
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth < 876) {
-        setIsOpen(false); // Ẩn sidebar khi chiều rộng nhỏ hơn 876px
+        setIsOpen(false);
       } else {
-        setIsOpen(true); // Hiện sidebar nếu lớn hơn hoặc bằng 876px
+        setIsOpen(true);
       }
     };
 
-    // Gọi hàm kiểm tra kích thước khi component được mount
     handleResize();
-
-    // Thêm sự kiện resize
     window.addEventListener("resize", handleResize);
-
-    // Cleanup event listener khi component unmount
     return () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   return (
-    <div className="h-full min-h-screen w-full">
+    <div className="h-full min-h-screen w-full overflow-hidden">
+      {" "}
+      {/* Thêm overflow-hidden nếu cần */}
       <Grid2 container sx={{ width: "100%" }}>
         {/* Header full màn hình */}
         <Grid2 item xs={12}>
           <AdminHeader toggleSidebar={toggleSidebar} user={user} />
         </Grid2>
-        <Grid2 container xs={12}>
-          {/* Sidebar chiếm 3 cột nếu mở, 0 cột nếu đóng, 1 cột cho màn hình nhỏ */}
+        <Grid2 container xs={12} sx={{ flexGrow: 1 }}>
+          {" "}
+          {/* Thêm flexGrow để chiếm không gian còn lại */}
           <Grid2 item xs={isOpen ? 3 : 0} sm={isOpen ? 3 : 1} sx={{ transition: "width 0.3s" }}>
             <AdminSideBar isOpen={isOpen} isOpenProp={isOpenProp} setIsOpenProps={setIsOpenProps} />
           </Grid2>
           {/* Nội dung chính chiếm 9 cột nếu sidebar mở, 12 cột nếu sidebar đóng */}
-          <Grid2 item xs={isOpen ? 9 : 12} sm={isOpen ? 9 : 10} sx={{ transition: "width 0.3s" }}>
+          <Grid2
+            item
+            xs={isOpen ? 9 : 12}
+            sm={isOpen ? 9 : 11}
+            sx={{
+              transition: "width 0.3s",
+            }}
+          >
             <Outlet />
           </Grid2>
         </Grid2>
